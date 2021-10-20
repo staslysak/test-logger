ae3a8e8a38661b8db6044a403b4bc59ff34e3678
Stas Lysak <staslyzak@gmail.com>
2021-10-20 14:23:25 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,19 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
 app.post('/', (req, res) => {
-  console.log('POST', JSON.stringify(req.body, null, 2))
+  console.log('---POST---')
+  console.log('params', JSON.stringify(req.params, null, 2))
+  console.log('query', JSON.stringify(req.query, null, 2))
+  console.log('body', JSON.stringify(req.body, null, 2))
+  
   res.sendStatus(200)
 })
 app.get('/', (req, res) => {
-  console.log('GET', JSON.stringify(req.body, null, 2))
+  console.log('---GET---')
+  console.log('params', JSON.stringify(req.params, null, 2))
+  console.log('query', JSON.stringify(req.query, null, 2))
+  console.log('body', JSON.stringify(req.body, null, 2))
+ 
   res.sendStatus(200)
 })
 
